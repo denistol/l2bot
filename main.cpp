902c19a0de4438fa2fb05a855f04c6f87a5b2e46
denistol <denistol@pm.me>
2025-01-28 04:35:03 +0200

    Added example

--- main.cpp
+++ main.cpp
@@ -1,57 +1,143 @@
-
+#include <fmt/core.h>
 #include <stdio.h>
-#include <curl/curl.h>
- 
-int main(void)
+#include <vector>
+#include <opencv4/opencv2/opencv.hpp>
+
+typedef std::vector<cv::Point2f> Targets;
+
+enum BotState
 {
-  CURL *curl;
-  CURLcode res;
- 
-  curl_global_init(CURL_GLOBAL_DEFAULT);
- 
-  curl = curl_easy_init();
-  if(curl) {
-    curl_easy_setopt(curl, CURLOPT_URL, "https://example.com/");
- 
-#ifdef SKIP_PEER_VERIFICATION
-    /*
-     * If you want to connect to a site who is not using a certificate that is
-     * signed by one of the certs in the CA bundle you have, you can skip the
-     * verification of the server's certificate. This makes the connection
-     * A LOT LESS SECURE.
-     *
-     * If you have a CA cert for the server stored someplace else than in the
-     * default bundle, then the CURLOPT_CAPATH option might come handy for
-     * you.
-     */
-    curl_easy_setopt(curl, CURLOPT_SSL_VERIFYPEER, 0L);
-#endif
- 
-#ifdef SKIP_HOSTNAME_VERIFICATION
-    /*
-     * If the site you are connecting to uses a different host name that what
-     * they have mentioned in their server certificate's commonName (or
-     * subjectAltName) fields, libcurl refuses to connect. You can skip this
-     * check, but it makes the connection insecure.
-     */
-    curl_easy_setopt(curl, CURLOPT_SSL_VERIFYHOST, 0L);
-#endif
- 
-    /* cache the CA cert bundle in memory for a week */
-    curl_easy_setopt(curl, CURLOPT_CA_CACHE_TIMEOUT, 604800L);
- 
-    /* Perform the request, res gets the return code */
-    res = curl_easy_perform(curl);
-    /* Check for errors */
-    if(res != CURLE_OK)
-      fprintf(stderr, "curl_easy_perform() failed: %s\n",
-              curl_easy_strerror(res));
- 
-    /* always cleanup */
-    curl_easy_cleanup(curl);
+  IS_SEARCHING,
+  IS_HOVERED,
+  IS_SELECTED,
+  IS_ATTACK,
+  IS_PICKUP
+};
+
+class Bot
+{
+public:
+  bool is_running = false;
+  BotState state;
+  Targets targets = {};
+
+  Bot()
+  {
   }
- 
-  curl_global_cleanup();
- 
+
+  Targets get_targets()
+  {
+    this->targets = {};
+    return this->targets;
+  }
+
+  bool is_hovered()
+  {
+  }
+  bool is_selected()
+  {
+  }
+
+  // Actions
+
+  void search_target() {
+    fmt::println("Searching...");
+    // if is search.
+    this->state = BotState::IS_HOVERED;
+  }
+
+  void select_target() {
+    this->state = BotState::IS_SELECTED;
+  }
+
+  void attack_target() {
+    fmt::println("Bot attack!");
+    this->state = BotState::IS_ATTACK;
+  }
+
+  // Main loop
+
+  void run()
+  {
+    while (true)
+    {
+      switch (this->state)
+      {
+      case BotState::IS_SEARCHING:
+        this->search_target();
+        break;
+      case BotState::IS_HOVERED:
+        this->select_target();
+        break;
+      case BotState::IS_SELECTED:
+        this->attack_target();
+        break;
+      case BotState::IS_ATTACK:
+        // check hp...
+        break;
+      case BotState::IS_PICKUP:
+        fmt::println("Bot searching...");
+        break;
+
+      default:
+        fmt::println("Bot default...");
+      }
+    }
+  }
+};
+
+cv::Point2f detect(cv::Mat &img, cv::Mat &templ)
+{
+  cv::Mat result;
+
+  // Methods
+
+  // cv::TM_SQDIFF;
+  // cv::TM_SQDIFF_NORMED;
+  // cv::TM_CCORR;
+  // cv::TM_CCORR_NORMED;
+  // cv::TM_CCOEFF;
+  // cv::TM_CCOEFF_NORMED;
+
+  cv::TemplateMatchModes method = cv::TM_CCOEFF_NORMED;
+
+  int result_cols = img.cols - templ.cols + 1;
+  int result_rows = img.rows - templ.rows + 1;
+
+  result.create(result_rows, result_cols, CV_32FC1);
+
+  matchTemplate(img, templ, result, method);
+  fmt::println("{}", result.cols);
+  normalize(result, result, 0, 1, cv::NORM_MINMAX, -1, cv::Mat());
+
+  double minVal, maxVal;
+  cv::Point minLoc, maxLoc, matchLoc;
+
+  cv::minMaxLoc(result, &minVal, &maxVal, &minLoc, &maxLoc, cv::Mat());
+
+  matchLoc = (method == cv::TM_SQDIFF || method == cv::TM_SQDIFF_NORMED) ? minLoc : maxLoc;
+
+  cv::Point from = matchLoc;                                                  // x1 y1
+  cv::Point to = cv::Point(matchLoc.x + templ.cols, matchLoc.y + templ.rows); // x2 y2
+
+  cv::Point2f center = cv::Point2f((from.x + to.x) / 2, (from.y + to.y) / 2);
+
+  fmt::println("{} : {}", center.x, center.y);
+
+  return center;
+}
+
+int main()
+{
+  cv::Mat img = cv::imread("../img/img.jpg", cv::IMREAD_GRAYSCALE);
+  cv::Mat templ = cv::imread("../img/target_hovered.jpg", cv::IMREAD_GRAYSCALE);
+  cv::Point2f result = detect(img, templ);
+  auto color = cv::Scalar(0, 255, 100);
+
+  cv::circle(img, result, 20, color, 5);
+
+  cv::imshow("img", img);
+  cv::waitKey(0);
+
   return 0;
 }
